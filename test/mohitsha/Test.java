98c9257f6b6ca07ce7b1c2178017027a6a90b014
mohit <mohitsha@thoughtworks.com>
2014-01-25 16:01:26 +0530

    My first test in lazybones.

---
+++ test/mohitsha/Test.java
@@ -0,0 +1,13 @@
+package mohitsha;
+
+import junit.framework.Assert;
+
+/**
+ * Created by mohitsha on 1/25/14.
+ */
+public class Test {
+    @org.junit.Test
+    public void testName() throws Exception {
+        Assert.assertEquals("Hello","Hello");
+    }
+}
